d2f86ab75124e34a5d36f92b42acef1ca29b4566
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-11-26 19:46:13 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main() {
   
-  int angka;
-  cout << "Masukan Angka :";
+  int angka;  //deklarasi angka
+  cout << "Masukan Angka :"; //output masukan angka
   cin >> angka; //input angka
 
   int fak = 1; //deklarasi faktorial
 
   for(int x = 1; x <= angka; x++) //perulangan for dimulai dari x = 1 sampai dengan x kurang dari atau sama dengan angka, nilai x akan naik 1 angka.
 {
-  fak = fak * x; 
+  fak = fak * x; // rumus mencari faktorial
 }
 
-cout << "faktorial of :" << angka << " = " << fak;
+cout << "faktorial of :" << angka << " = " << fak; //untuk menampilkan hasil faktorial dari input yg dimasukan
 
   return 0;
 } 
